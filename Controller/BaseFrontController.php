cea685620ad7fd586f0c626f018d2341be5a263c
Stephen Wilkinson <du_monde@hotmail.com>
2014-07-29 15:34:44 +0000

    [Core] Added Base FrontController and event listener to auto-load page entities

---
+++ Controller/BaseFrontController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Stems\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * This is the base front controller for all Stems-compatible modules. Primarily for auto-loading functionality to integrate with CMS controlled paging.
+ */
+class BaseFrontController extends Controller
+{
+	/**
+	 * A container for the page entity loaded from the CMS.
+	 * The event handler will attempt a best guess at loading the page from the CMS, but will create a default page entity as a fallback.
+	 */
+	public $page;
+
+	/**
+	 * A pointer for the entity manager, because we'll always need to load it for front end page it cuts down on controller fat by having it here.
+	 */
+	public $em;
+}
---
+++ EventListener/FrontListener.php
@@ -0,0 +1,61 @@
+<?
+
+namespace Stems\CoreBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException,
+    Symfony\Component\HttpKernel\Event\FilterControllerEvent,
+    Doctrine\ORM\EntityManager;
+
+use Stems\CoreBundle\Controller\BaseFrontController,
+    Stems\PageBundle\Entity\Page;
+
+class FrontListener
+{
+    /**
+     * To hold the injected entity manager
+     */
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $controller = $event->getController();
+
+        // the controller should come in as an array, so get it out first
+        if (is_array($controller)) {
+            $controller = $controller[0];
+        } else {
+            return;
+        }
+
+        // only run if the controller loaded is an extension of the Stems base front controller
+        if ($controller instanceof BaseFrontController) {
+
+            // inject the entity manager into the controller
+            $controller->em = $this->em;
+
+            // get the path so we can attempt to predict the page that's needed
+            $path = ltrim($event->getRequest()->getPathInfo(), '/');
+
+            // attempt to load the page using an absolute path match first
+            try
+            {
+                $controller->page = $this->em->getRepository('StemsPageBundle:Page')->load($path);
+            }
+            // if the absolute path doesn't work due to dynamix slug components, then attempt estimate the page
+            catch (\Exception $e) 
+            {
+                $controller->page = $this->em->getRepository('StemsPageBundle:Page')->estimate($path);
+
+                // if all else fails it probably doesn't exist, so create a generic page object that uses the default layout
+                if (!is_object($controller->page)) {
+                    $controller->page = new Page();    
+                }
+            }
+        }
+    }
+}
