35f0cc3bbacf87c8d25a08b316c1ef1ba40c62b0
Stephen Wilkinson <du_monde@hotmail.com>
2014-08-07 16:14:49 +0000

    [Blog] Added username filter to admin overview

---
+++ Twig/CoreExtension.php
@@ -0,0 +1,58 @@
+
+<?php
+
+namespace Stems\CoreBundle\Twig;
+
+use Doctrine\ORM\EntityManager;
+
+class CoreExtension extends \Twig_Extension
+{
+	/**
+	 * The entity manager
+	 */
+	protected $em;
+
+	public function __construct(EntityManager $em)
+	{
+		$this->em = $em;
+	}
+
+	public function getFilters()
+	{
+		return array(
+			new \Twig_SimpleFilter('username', array($this, 'usernameFilter')),
+		);
+	}
+
+	public function getName()
+	{
+		return 'stems_core_extension';
+	}
+
+	/**
+	 * Converts the given user id into it's associated name format, defaulting to the fullname
+	 *
+	 * @param  integer  $id         The user id
+	 * @param  string   $format     The name format to use, either forname, surname, fullname or username
+	 * @return string               The request name
+	 */
+	public function usernameFilter($id, $format='fullname')
+	{
+		// get the user entity
+		$user = $this->em->getRepository('StemsUserBundle:User')->find($id);
+
+		// skip if we can't find the user
+		if (!is_object($user)) {
+			return $id;
+		}
+
+		// build the get method and call it if it exists
+		$method = 'get'.ucfirst($format);
+
+		if (method_exists($user, $method)) {
+			return $user->$method();
+		} else {
+			return $id;
+		}
+	}	
+}
