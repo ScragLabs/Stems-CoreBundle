89ea5c839a1cf957f1038aea0c38af9e340aa459
Stephen Wilkinson <du_monde@hotmail.com>
2014-09-11 12:47:50 +0000

    Basic frontend js for Core

--- Resources/public/js/frontend.js
+++ Resources/public/js/frontend.js
@@ -1,4 +1,27 @@
+/**
+ * Create flash message
+ */
+function createFlashMessage(type, message) {
+
+	// Remove any existing messages
+	$('.flash').remove();
+
+	// Create the new message
+	var element = $('<div class="flash flash-'+type+'"><p>'+message+'</p><i class="fa close">&times;</i></div>');
+	$('#flash-messages > .wrap').append(element);
+
+	// Auto hide after 5 seconds
+	setTimeout(function() {
+		element.slideToggle();
+	}, 5000);
+}
+
 $(document).ready(function() {
 	
-	
+	/**
+	 * Close flash message
+	 */
+	$('body').on('click', '.flash .close', function(){
+		$(this).parent().slideToggle();
+	});
 });
